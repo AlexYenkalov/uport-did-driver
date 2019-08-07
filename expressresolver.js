41dc740833c8a4140b4111e0c080295209585c5b
Alexander Yenkalov <alexander.yenkalov@gmail.com>
2019-08-07 22:39:53 +0300

    added spherity-did-resolver and 7hirten-did-resolver

--- expressresolver.js
+++ expressresolver.js
@@ -4,6 +4,8 @@ const uportResolver = require('uport-did-resolver')
 const muportResolver = require('muport-did-resolver')
 const ethrResolver = require('ethr-did-resolver')
 const ethResolver = require('eth-did-resolver')
+const spherityResolver = require('spherity-did-resolver')
+const siebenhirtenResolver = require('7hirthen-did-resolver')
 
 const express = require('express')
 const app = express()
@@ -13,6 +15,8 @@ uportResolver()
 muportResolver()
 ethrResolver()
 ethResolver()
+spherityResolver()
+siebenhirtenResolver()
 
 app.get('/1.0/identifiers/*', function (req, res) {
 
@@ -37,3 +41,4 @@ var server = app.listen(8081, function () {
 // did:eth:0x3b0BC51Ab9De1e5B7B6E34E5b960285805C41736
 // did:ethr:0x3b0BC51Ab9De1e5B7B6E34E5b960285805C41736
 // did:uport:2omWsSGspY7zhxaG6uHyoGtcYxoGeeohQXz
+// did:spherity:0x10580e0a5efeadcd6fb9a4f78a5fab95a3e92a9e
